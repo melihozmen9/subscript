15b4537904d3f488ca68ec09e03c85d0cee9fdd3
melihozmen9 <105754659+melihozmen9@users.noreply.github.com>
2022-07-04 13:53:02 +0300

    subscript1

--- subscript/ViewController.swift
+++ subscript/ViewController.swift
@@ -12,8 +12,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let result = hesapla(carpan: 3)
+        print("6 kere 3 = \(result[6])")
+        
+        
+        var event = Event()
+        print("3. index değeri = \(event[2])")
+        
+        event[2] = "yüzme"
+        print("3. index değeri = \(event[2])")
     }
 
 
 }
 
+struct hesapla {
+    let carpan : Int
+    
+    subscript(satirsayisi:Int) -> Int{
+        return carpan*satirsayisi  //geriye değer döndüreceği için buraya return yazdık
+        
+    }
+}
+
+struct Event{
+    var events = ["futbol","konser","sinema","tiyatro"]
+    
+    subscript(eventindex:Int)-> String{
+        get{
+            return events[eventindex]
+        }
+        set(newvalue){
+          events[eventindex] = newvalue
+        }
+    }
+}
